3ebce90fc52f9344c445f1d0bb04dcbce8065bf5
divishtk <divisht.avion@hotmail.com>
2025-06-02 23:02:43 +0530

    refactor: update Login component layout and structure

--- src/login.jsx
+++ src/login.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>login</div>
-  )
-}
+    <div className="flex justify-center my-15">
+    <div className="card bg-base-200 w-96 shadow-sm">
+      <div className="card-body">
+        <h2 className="card-title">Login</h2>
+        <input type="text" placeholder="Email" className="input rounded-sm" />
+        <input type="password" placeholder="Password" className="input rounded-sm" />
+        <div className="card-actions justify-end mt-3">
+          <button className="btn btn-primary">Log In</button>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
